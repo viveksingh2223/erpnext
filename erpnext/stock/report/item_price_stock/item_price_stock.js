b0593324081c7729d6bba6c2fe886eac61503fa4
Sahil Khan <sahilkhan28297@gmail.com>
2020-06-01 15:39:40 +0530

    Merge branch 'v11-pre-release' into version-11

---
+++ erpnext/stock/report/item_price_stock/item_price_stock.js
@@ -0,0 +1,19 @@
+// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Item Price Stock"] = {
+	"filters": [
+		{
+			"fieldname":"item_code",
+			"label": __("Item"),
+			"fieldtype": "Link",
+			"options": "Item",
+			"get_query": function() {
+				return {
+					query: "erpnext.controllers.queries.item_query"
+				}
+			}
+		}
+	]
+}
